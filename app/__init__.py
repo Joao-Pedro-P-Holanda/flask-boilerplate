90112712dbc56ee0ed4bba1f16155c3e4f8c6b75
Joao-Pedro-P-Holanda <joaopedroph.dev@gmail.com>
2025-03-28 20:23:18 -0300

    feat: add security base configuration
    add configuration for https only when running on production mode,
    default Content-Security-Policy, Http cookies and etc with
    Flask-Talisman base config
    
    add CSRF security with Flask-Seasurf
    
    following redirects in text files

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
-from flask import Flask, current_app, jsonify
+from datetime import timedelta
+from flask import Flask
 from flask_migrate import Migrate
+from flask_talisman import Talisman
+from flask_seasurf import SeaSurf
 from .models import config_db
 from .serializer import config_ma
 from .blueprints.hello_world import hello_world_bp
@@ -7,23 +10,30 @@ from dotenv import load_dotenv
 import os
 
 
-def route_not_found(e):
-    #change this method to your prefered way of handling errors
+def route_not_found(e: Exception):
+    # change this method to your prefered way of handling errors
     return "can't resolve url", 404
 
 
 def create_app():
-    #flask application created on the factory method
+    # flask application created on the factory method
     app = Flask(__name__)
-    #setting SQLAlchemy database using the environment URI 
+    # setting SQLAlchemy database using the environment URI
     load_dotenv()
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
-    #configure the database and migrations
+    app.secret_key = os.getenv("SECRET_KEY")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URI")
+    # configure the database and migrations
     config_db(app)
-    config_ma(app)
     migrate = Migrate(app, app.db)
-    #register all blueprints and errors routes
-    app.register_blueprint(hello_world_bp)
-    app.register_error_handler(404,route_not_found)
-    return app
 
+    # serialization
+    config_ma(app)
+    # Security headers and middlewares
+    _ = Talisman(app)
+    _ = SeaSurf(app)
+
+    # register all blueprints and errors routes
+    app.register_blueprint(hello_world_bp)
+    app.register_error_handler(404, route_not_found)
+
+    return app
--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -1,6 +1,4 @@
-
 def test_get_hello_page(client):
-    response = client.get("/")
+    response = client.get("/", follow_redirects=True)
     # check if a 200 status code is returned
     assert response.status_code == 200
-
